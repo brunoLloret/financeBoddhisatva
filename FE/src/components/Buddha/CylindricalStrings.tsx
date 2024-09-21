0c08bc4a510c2b8e6ce91a3122618f6f45772257
Bruno Lloret <caballoret@gmail.com>
2024-09-21 10:02:54 -0700

    removed console.log(animating...

--- FE/src/components/Buddha/CylindricalStrings.tsx
+++ FE/src/components/Buddha/CylindricalStrings.tsx
@@ -37,7 +37,7 @@ const CylindricalStrings: React.FC<CylindricalStringsProps> = ({
 
     function animate() {
       const time = Date.now() * speed;
-      console.log("Animating at time:", time);
+      // console.log("Animating at time:", time);
 
       stringRefs.current.forEach((ref, index) => {
         if (ref) {
